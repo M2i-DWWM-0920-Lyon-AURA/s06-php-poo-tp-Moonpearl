d907fa2a4ef826adaf1ad545bb265e173fe36835
Romain Tello <romain.tello@gmail.com>
2020-10-23 11:58:09 +0200

    Add Platform model and refactor common code as abstract class

---
+++ models/AbstractBrand.php
@@ -0,0 +1,75 @@
+<?php
+
+abstract class AbstractBrand
+{
+    protected $id;
+    protected $name;
+    protected $link;
+
+    /**
+     * Create new AbstractBrand object
+     * 
+     * @param int $id Database ID
+     * @param string $name AbstractBrand name
+     * @param string $link Link to brand's website
+     */
+    public function __construct(int $id = null, string $name = '', string $link = '')
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->setLink($link);
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of link
+     */ 
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set the value of link
+     *
+     * @return  self
+     */ 
+    public function setLink($link)
+    {
+        // Vérifie que la nouvelle valeur reçue est bien une URL
+        if (!filter_var($link, FILTER_VALIDATE_URL)) {
+            throw new Exception('Developer::link property must be a valid URL.');
+        }
+
+        $this->link = $link;
+
+        return $this;
+    }
+}
--- models/Platform.php
+++ models/Platform.php
@@ -2,7 +2,7 @@
 
 require_once './models/AbstractBrand.php';
 
-class Developer extends AbstractBrand
+class Platform extends AbstractBrand
 {
     /**
      * Fetch all developers from database as Developer objects
@@ -10,14 +10,15 @@ class Developer extends AbstractBrand
     public static function fetchAll() {
         global $databaseHandler;
     
-        $statement = $databaseHandler->query('SELECT * FROM `developer`');
-        return $statement->fetchAll(PDO::FETCH_FUNC, 'Developer::create');
+        $statement = $databaseHandler->query('SELECT * FROM `platform`');
+        return $statement->fetchAll(PDO::FETCH_FUNC, 'Platform::create');
     }
 
     /**
      * Convert `developer` record from database to Developer object
      */
     public static function create($id, $name, $link) {
-        return new Developer($id, $name, $link);
+        return new Platform($id, $name, $link);
     }
+
 }
